a985a97dd6094b5105ab7aa95e6588ebcae4464b
Antony Castineiras <antonycastineiras@gmail.com>
2024-12-05 16:27:20 +0000

    Updated latest-version script

--- scripts/get-latest-cmk-version.py
+++ scripts/get-latest-cmk-version.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
 import sys
+import requests
+import json
 
-string = sys.argv[1]
+try:
+    version = sys.argv[1]
+except IndexError as e:
+    print("No Version Provided")
+    sys.exit(2)
 
-if 'p' in string:
-    print(string)
+def get_latest_version(version):
+    url = "https://download.checkmk.com/stable_downloads.json"
+    res = requests.get(url)
+    data = json.loads(res.text)
+
+    try:
+        latest_version = data['checkmk'][version]['version']    
+    except Exception as ex:
+        print(f"No JSON Data Returned For Version: {version}")
+        sys.exit(2)
+
+    return latest_version
+
+if 'p' in version:
+    print(version)
 else:
-    print(f"p not in {string}")
+    print(get_latest_version(version))
