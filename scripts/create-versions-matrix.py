34994bd50bd84c0c1bd5531ee60f88d431e7ba3e
Antony Castineiras <antonycastineiras@gmail.com>
2024-12-06 22:29:32 +0000

    Updated matrix

---
+++ scripts/create-versions-matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import requests
+import json
+
+url = "https://download.checkmk.com/stable_downloads.json"
+res = requests.get(url)
+data = json.loads(res.text)
+
+try:
+    versions_arg = sys.argv[1]
+except IndexError as e:
+    print("No Version Provided")
+    sys.exit(2)
+
+versions = versions_arg.split(",")
+
+matrix = {"include": [ {"version": data["checkmk"][version]["version"]} for version in versions ]} 
+json_matrix = json.dumps(matrix)
+print(json_matrix)
